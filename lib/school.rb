8d9f9cf974835149694d7e8e05bb767cacd71b4b
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-29 18:30:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  
+  attr_accessor :name
+  
+  def initialize(name, roster)
+    @name = name
+    @roster = roster{}
+  end
+  
+  def name
+    @name
+  end
+  
+  def roster
+    @roster
+  end
+  
+end
